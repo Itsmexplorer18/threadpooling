23a328a0161e760edbb2040abdfaaf9013d56085
Itsmexplorer18 <124561481+Itsmexplorer18@users.noreply.github.com>
2025-03-14 23:26:30 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-//threadpooling implementation with generic programming
 #include <iostream>
 #include <thread>
 #include <mutex>
@@ -6,113 +5,105 @@
 #include <vector>
 #include <queue>
 #include <functional>
-#include <future> 
+#include <future>
+#include <chrono>
+
 using namespace std;
 
-class ThreadPool{
- private:
-   int m_threads;
-   vector<thread> threads;
-   queue<function<void()>> tasks;
-   mutex mtx;
-   condition_variable cv;
-   bool stop;
- public:
-   explicit ThreadPool(int numthreads):m_threads{numthreads},stop{false}{
-      for(int i=0;i<m_threads;i++){
-         threads.emplace_back([this](){
+class ThreadPool {
+private:
+    int m_threads;
+    vector<thread> threads;
+    queue<function<void()>> tasks;
+    mutex mtx;
+    condition_variable cv;
+    bool stop;
+
+public:
+    explicit ThreadPool(int numthreads) : m_threads{numthreads}, stop{false} {
+        for (int i = 0; i < m_threads; i++) {
+            threads.emplace_back([this]() {
+                while (true) {
                     function<void()> currtask;
-            while(1){
-               unique_lock<mutex> lock(mtx);
-               cv.wait(lock,[this]{
-                return !tasks.empty() or stop;
-               });
-               if(stop) return;
-               currtask=move(tasks.front());
-               tasks.pop();
-               lock.unlock(); //why unlock?
-               currtask();
-            }
-         });
-      }
-   }
-    ~ThreadPool(){
-        //we want wait for all curr running threads to be finished and stop value =true to notify all threads to np more take tasks
-        unique_lock<mutex> lock(mtx);
-        stop=true;
-        lock.unlock();
+                    unique_lock<mutex> lock(mtx);
+                    cv.wait(lock, [this] {
+                        return !tasks.empty() || stop;
+                    });
+                    if (stop) return;
+                    currtask = move(tasks.front());
+                    tasks.pop();
+                    lock.unlock(); // Unlock before running the task to allow other threads to pick tasks
+                    currtask();
+                }
+            });
+        }
+    }
+
+    ~ThreadPool() {
+        {
+            unique_lock<mutex> lock(mtx);
+            stop = true;
+        }
         cv.notify_all();
-        for(auto &th:threads){
+        for (auto &th : threads) {
             th.join();
         }
     }
+
     template<class F, class... Args>
-auto executetasks(F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
-    using return_type = decltype(f(args...));
-
-    // Create a shared_ptr for packaged_task
-    auto task = std::make_shared<std::packaged_task<return_type()>>(
-        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-    );
-
-    // Get the future associated with the task
-    std::future<return_type> res = task->get_future();
-
-    // Add the task to the queue
-    {
-        std::unique_lock<std::mutex> lock(mtx);
-        tasks.emplace([task]() {
-            (*task)(); // Correctly dereference the shared_ptr and execute the task
-        });
-    }
-
-    // Notify one thread to pick up the task
-    cv.notify_one();
-
-    return res;
-}
-
-   /* template<class F,class... Args>
-    auto  executetasks(F&& f,Args&&... args)->future<decltype(f(args...))>{
-        using return_type=decltype(f(args...));
-        auto task=make_shared<packaged_task<return_type()>>(bind(forward<F>(f),forward<Args>(args...)));
-        future<return_type> res=task->get_future();
-        unique_lock<mutex> lock(mtx);
-        tasks.emplace([task]()->void{
-            *(task)();
-        });
-        lock.unlock();
+    auto executetasks(F&& f, Args&&... args) -> future<decltype(f(args...))> {
+        using return_type = decltype(f(args...));
+        auto task = make_shared<packaged_task<return_type()>>(bind(forward<F>(f), forward<Args>(args)...));
+        future<return_type> res = task->get_future();
+        {
+            unique_lock<mutex> lock(mtx);
+            tasks.emplace([task]() {
+                (*task)();
+            });
+        }
         cv.notify_one();
         return res;
-    }*/
-   /* void executetasks(F f,Args... args){  //this is for multiple type of functions diff func may take diff no of arguments
-    //now we want return type to be the return type of the function trailing return type 
-        unique_lock<mutex> lock(mtx);
-        tasks.push(func);
-        lock.unlock();
-        cv.notify_one();
     }
-    */
-   /* void executetasks(function<void()> func){
-        unique_lock<mutex> lock(mtx);
-        tasks.push(func);
-        lock.unlock();
-        cv.notify_one();*/
-    };
+};
 
-//};
-int func(int a){
-    this_thread::sleep_for(chrono::seconds(2));
-    cout<<"some tasks of user"<<endl;
-    return a*a;
+// Function that takes time to execute
+int func(int a) {
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << "Task executed by thread ID: " << this_thread::get_id() << endl;
+    return a * a;
 }
-int main(){
-            ThreadPool pool(8);
-            future<int> res=pool.executetasks(func,1);
-            cout<<res.get();
-    while(1){
-       // pool.executetasks(func);
-       //cout<<"happening"<<endl;
+
+int main() {
+    int numTasks =8;
+    cout << "Number of tasks: " << numTasks << endl;
+    
+    // Sequential execution without thread pool
+    auto start_seq = chrono::steady_clock::now();
+    for (int i = 0; i < numTasks; i++) {
+        func(i);
     }
+    auto end_seq = chrono::steady_clock::now();
+    cout << "Time taken without thread pool: " 
+         << chrono::duration_cast<chrono::milliseconds>(end_seq - start_seq).count() 
+         << " milliseconds" << endl;
+
+    // Execution using thread pool
+    ThreadPool pool(4);  // Using 4 threads
+    vector<future<int>> results;
+
+    auto start_pool = chrono::steady_clock::now();
+    for (int i = 0; i < numTasks; i++) {
+        results.push_back(pool.executetasks(func, i));
+    }
+    
+    for (auto &res : results) {
+        res.get();  // Wait for all tasks to complete
+    }
+    
+    auto end_pool = chrono::steady_clock::now();
+    cout << "Time taken with thread pool: " 
+         << chrono::duration_cast<chrono::milliseconds>(end_pool - start_pool).count() 
+         << " milliseconds" << endl;
+
     return 0;
 }
